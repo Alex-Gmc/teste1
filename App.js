8a5e8fb0cf22a14c3cc98197911e1acc51719c6f
Alex-gmc <magrinfreestyle@gmail.com>
2022-05-30 22:15:19 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './componentes/title'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto} > Alexsandro Gumercindo</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#555'
   },
+  texto: {
+    fontSize: 30,
+    color: '#fff'
+  }
 });
